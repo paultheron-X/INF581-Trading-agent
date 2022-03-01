1cc08720f339aa52a9ca3143999872c1dde98bd5
Louis Proffit <louis.proffit@renault.polytechnique.fr>
2022-03-01 19:16:21 +0100

    Refactoring code, separating agent and env, analytics

--- gym_trading_btc/gym_anytrading/envs/env_scorer.py
+++ gym_trading_btc/gym_anytrading/envs/env_scorer.py
@@ -1,59 +1,100 @@
-from .bitcoin_env import CryptoEnv
+from cgi import print_form
 
+from models.agent import Agent
+from .bitcoin_env import CryptoEnv,Actions
+import numpy as np
+import random as rand
 
-class CryptoEnv_scorer():
-    def __init__(self, df, window_size, frame_len) -> None:
-        
-        self._actual_env = CryptoEnv(df, window_size, frame_len, name = 'test')
-        
-        self._random_env = CryptoEnv(df, window_size, frame_len, name = 'random')
-        self._random_reward_ep = 0
-        self._random_profit = 0
-        
-        self._optimal_env = CryptoEnv(df, window_size, frame_len, name = 'optimal')
-        self._optimal_reward_ep = 0
-        self._optimal_profit = 0
+class CryptoEnvScorer():
+
+    def __init__(self, env, agent):
+        self.env = env
+        self.agent = agent
+        self.random = Agent()
         
 
-    def reset(self):
-        obs = self._actual_env.reset()
-        self._random_env.reset()
-        self._optimal_env.reset()
-        
-        
-        self._random_env.merge(self._actual_env)
-        self._optimal_env.merge(self._actual_env)
-        
-        self._random_reward_ep = 0
-        self._random_profit = 0
-        
-        self._optimal_reward_ep = 0
-        self._optimal_profit = 0
-        
-        return obs
-    
-    def get_observation(self):
-        return self._actual_env._get_observation()
-    
-    def step(self, action):
-        
-        random_action = self._random_env.action_space.sample()
-        observation, reward, done, info = self._random_env.step(random_action)
-        self._random_reward_ep += reward
-        if not done:
-            self._random_profit += reward
-        
-        observation, reward, done, info = self._optimal_env.best_action()
-        self._optimal_reward_ep += reward
-        if not done:
-            self._optimal_profit += reward
-                
-        observation, reward, done, info = self._actual_env.step(action=action)
-                
-        return observation, reward, done, info
-    
-    def get_reward(self):
-        return self._random_reward_ep, self._optimal_reward_ep
-    
-    def get_profit(self):
-        return self._random_profit, self._optimal_profit
+    def play_episodes(self, num_episodes):
+        random_profits = []
+        agent_profits = []
+        optimal_profits = []
+        for i in range(num_episodes):
+            random_profit, agent_profit, optimal_profit = self.play_episode()
+            random_profits.append(random_profit)
+            agent_profits.append(agent_profit)
+            optimal_profits.append(optimal_profit)
+            print(f"> Episode {i + 1:5}  | random  {random_profit:10.2f}  | agent  {agent_profit:10.2f}  | optimal  {optimal_profit:10.2f}")
+        return random_profits, agent_profits, optimal_profits
+
+
+    def play_episode(self):
+        self.env.reset()
+        tick = self.env._padding_tick
+        random_profit = self.play_agent(self.random)
+        self.env.reset_to(tick)
+        agent_profit = self.play_agent(self.agent)
+        self.env.reset_to(tick)
+        optimal_profit = self.play_optimal()
+
+        return random_profit, agent_profit, optimal_profit
+
+    def play_random(self):
+        done = False
+        reward = None
+        while not(done):
+            random_action = self.env.action_space.sample()
+            _, reward, done, _ = self.env.step(random_action)
+        return reward
+
+    def play_optimal(self):
+
+        start = self.env._current_tick
+        steps = self.env.frame_len
+        end = start + steps
+        actions = [Actions.Stay] * steps
+        prices = self.env.prices[start:end]
+
+        def get_current_profit(prices, actions):
+            profit = 0
+            count = 0
+            for i in range(steps):
+                if (actions[i] == Actions.Buy):
+                    count += 1
+                    profit -= prices[i] # TODO fees
+                elif (actions[i] == Actions.Sell):
+                    count -= 1
+                    profit += prices[i] # TODO fees
+            profit += count * prices[steps-1]
+            return profit
+
+        profit = get_current_profit(prices, actions)
+
+        for i in range(3 * steps):
+            rand_index = rand.randint(0, steps - 1)
+            initial_action = actions[rand_index]
+            rand_action = initial_action
+            while(rand_action == initial_action):
+                rand_action = np.random.choice(list(Actions))
+            actions[rand_index] = rand_action
+            new_profit = get_current_profit(prices, actions)
+            if (new_profit <= profit):
+                actions[rand_index] = initial_action
+            else:
+                profit = new_profit
+            #print(profit)
+
+        return profit
+
+    def play_agent(self, agent):
+
+        state = self.env._get_observation()
+        terminal = False
+
+        while not terminal:
+
+            action = agent.predict(state)
+            state_after, reward, action, terminal = self.env.step(action)   
+            agent.learn(state, action, state_after, reward, terminal)
+
+            state = state_after
+
+        return reward
---
+++ models/agent.py
@@ -0,0 +1,19 @@
+
+
+class Agent: 
+
+    def __init__(self, **kwargs):
+         self.window_size = kwargs["window_size"]
+         self.num_features = kwargs["num_features"]
+         self.action_space = kwargs["num_actions"]
+         self.state_space = self.window_size * self.num_features
+        
+         
+    def predict(self, state):
+        return self.action_space.sample() # Should be overriden
+
+    def learn(self, previous_state, action, next_state, reward, terminal):
+        return # Random agent doesn't do anything
+
+    def print_infos(self):
+        print("Random agent")
