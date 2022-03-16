8e76b4db8ce7e856f141e30a7ceca5459bb033da
Jérémie Dentan <jeremie.dentan@live.com>
2022-03-16 15:25:16 +0100

    sinus

--- models/utils/sinus_generator.py
+++ models/utils/sinus_generator.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 import argparse
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--period", help="function period", default=5, required=False)
-parser.add_argument("--amplitude", help="function amplitude", default=10, required=False)
+parser.add_argument("--period", help="function period", default=0.2, required=False)
+parser.add_argument("--amplitude", help="function amplitude", default=50, required=False)
 parser.add_argument("--offset", help="function offset", default=900, required=False)
 args = parser.parse_args()
 
@@ -17,7 +18,7 @@ column_names = ["unix","date","symbol","open","high","low","close","Volume BTC",
 columns_pd = np.zeros((T,len(column_names)))
 
 signal = h + M*np.sin(l*0)
-for t in range(T):
+for t in tqdm(range(T)):
     newsignal = h + M*np.sin(l*t)
 
     unix = np.random.randint(100)
@@ -28,5 +29,7 @@ for t in range(T):
     volumeBTC = 20*np.random.random()
     volumeUSD = (1+np.random.random()/30)*signal*volumeBTC
     columns_pd[t] = np.array([unix, date, symbol, signal, high, newsignal, low,volumeBTC, volumeUSD])
+
+    signal = newsignal
 
 pd.DataFrame(columns_pd,columns=column_names).to_csv(f"gym_trading_btc/datasets/data/sinus_l={args.period}_M={args.amplitude}_h={args.offset}.csv",index=False)
