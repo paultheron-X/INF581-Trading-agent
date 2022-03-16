cf15d6bd82a4f6d24a58356de8004f4946715379
MeryJa <meryemjaaidan@gmail.com>
2022-03-16 16:50:15 +0100

    update main_pg

--- main_pg.py
+++ main_pg.py
@@ -1,8 +1,9 @@
 import gym
-from models.a2c.policygradient import *
+from models.policy_gradient.policygradient_torch import *
 from gym_trading_btc.gym_anytrading.envs.bitcoin_env import *
 import matplotlib.pyplot as plt
 import pandas as pd
+from config_mods import config_dqn_deepsense as config
 
 df_btc = pd.read_csv("gym_trading_btc/gym_anytrading/datasets/data/Bitstamp_BTCUSD_1h.csv", delimiter= ",")
 
@@ -11,9 +12,13 @@ frame_len = 6
 start_index = window_size
 end_index = len(df_btc)
 
-env = CryptoEnv(df = df_btc , window_size=window_size, frame_len= frame_len)
+env = CryptoEnv(**config)
+#agent = DQNAgentDeepsense(**config)
+#scorer = CryptoEnvScorer(env, agent, **config)
+
+#env = CryptoEnv(df = df_btc , window_size=window_size, frame_len= frame_len)
 env.seed(1)     # reproducible, general Policy gradient has high variance
-env = env.unwrapped
+#env = env.unwrapped
 
 print(env.action_space)
 print(env.observation_space)
@@ -21,29 +26,23 @@ print((env.observation_space.shape[0],))
 print(env.observation_space.high)
 print(env.observation_space.low)
 
-RL = PolicyGradient(
+RL = PolicyGradient()
+"""
     n_actions=env.action_space.n,
     n_features=env.observation_space.shape[0],
     learning_rate=0.02,
     reward_decay=0.99,
     # output_graph=True,
 )
+"""
 
 for i_episode in range(3000):
 
     observation = env.reset()
     #print(observation.shape)
     while True and observation.shape == (env.observation_space.shape[0],):
-        #and (observation.shape == env.observation_space.shape)
-        #print(str(observation.shape))
-        #if observation.shape == (env.observation_space.shape[0],):
         action = RL.choose_action(observation)
-        
-        if observation.shape == env.observation_space.shape:
-            action = RL.choose_action(observation)
-        else:
-            action = env.action_space.sample()
-        
+    
         observation_, reward, done, info = env.step(action)
         
         RL.store_transition(observation, action, reward)
@@ -58,13 +57,6 @@ for i_episode in range(3000):
             print("episode:", i_episode, "  reward:", int(running_reward))
 
             vt = RL.learn()
-            """
-            if i_episode == 2999:
-                plt.plot(vt)    # plot the episode vt
-                plt.xlabel('episode steps')
-                plt.ylabel('normalized state-action value')
-                plt.show()
-            """
             break
         observation = observation_
         #print(str(observation_.shape))
